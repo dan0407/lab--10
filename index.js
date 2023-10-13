da039353356d1454a62e18ff9294fca860f4b23a
Daniel Alejandro Duque Murillas <1025060503@u.icesi.edu.co>
2023-10-13 17:29:06 -0500

    3

---
+++ index.js
@@ -0,0 +1,37 @@
+let peliculas = []
+for(let i = 0; i < data.length; i++) {
+    const peliculas1 = data[i]
+    const p = new Peliculas( 
+        peliculas1.titulo, 
+        peliculas1.sinopsis, 
+        peliculas1.Image, 
+        peliculas1.rating,
+            )
+            peliculas.push(p)
+}      
+
+function renderAllPeliculas(peli) {
+    let container = document.getElementById("populares")
+    container.innerHTML = ""
+    for(let i = 0; i < peli.length; i++) {
+        const pe = peli[i]
+        container.innerHTML += pe.toHtml(i)
+    }
+}
+renderAllPeliculas(peliculas)
+
+function renderAllPeliculasestreno(peli) {
+    let container = document.getElementById("estreno")
+    container.innerHTML = ""
+    for(let i = 0; i < peli.length; i++) {
+        const pe = peli[i]
+        container.innerHTML += pe.toHtml(i)
+    }
+}
+renderAllPeliculasestreno(peliculas)
+
+function peliculaSelected(position){
+    var pelicula= peliculas[position]
+console.log(pelicula)
+} 
+
---
+++ peliculas.js
@@ -0,0 +1,22 @@
+class Peliculas {
+    constructor(titulo, sinopsis, Image, rating) {
+        this.titulo = titulo
+        this.sinopsis = sinopsis
+        this.Image = Image
+        this.rating = rating
+        }
+
+    toHtml(position) {
+        return `
+        <div onclick='peliculaSelected(${position})'><img src="${this.Image}" width="300" height="400" alt=""> 
+        <div class="estrellas">
+            <img src="./silueta-de-estrella-negra.png" width="32" alt="">
+            <img src="./silueta-de-estrella-negra.png" width="32" alt="">
+            <img src="./silueta-de-estrella-negra.png" width="32" alt="">
+            <img src="./silueta-de-estrella-negra.png" width="32" alt="">
+            <img src="./estrella.png" width="32" alt="">
+        </div>
+    </div>
+        `
+    }
+}
